b45f11bd10c799325126d3f317f3cd2e0af215cc
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-27 21:09:08 +0200

    Adding more lessons

--- HTSAS/HTSAS/ViewController.swift
+++ HTSAS/HTSAS/ViewController.swift
@@ -21,7 +21,19 @@ class ViewController: UIViewController, UITableViewDataSource {
         lessons.append(HTSASLesson(title: "Before the Startup", authors: "Paul Graham"))
         lessons.append(HTSASLesson(title: "Building Product, Talking Users and Growing", authors: "Adora Cheung"))
         lessons.append(HTSASLesson(title: "Competition is for losers", authors: "Peter Thiel"))
-        println(lessons)
+        lessons.append(HTSASLesson(title: "Growth", authors: "Alex Schultz"))
+        lessons.append(HTSASLesson(title: "How to Build Products Users Love", authors: "Kevin Hale"))
+        lessons.append(HTSASLesson(title: "Doing Things That Don't Scale", authors: "Press"))
+        lessons.append(HTSASLesson(title: "How To Raise Money", authors: "Marc Andreessen, Ron Conway, Parker Conrad"))
+        lessons.append(HTSASLesson(title: "Culture", authors: "Brian Chesky, Alfred Lin"))
+        lessons.append(HTSASLesson(title: "Hiring and Culture - Part 2", authors: "Patrik and John Collison, Ben Silbermann"))
+        lessons.append(HTSASLesson(title: "Building for the Enterprise", authors: "Aaron Levie"))
+        lessons.append(HTSASLesson(title: "How to Operate", authors: "Keith Rabois"))
+        lessons.append(HTSASLesson(title: "How to Manage", authors: "Ben Horowitz"))
+        lessons.append(HTSASLesson(title: "How to Run a User Interview", authors: "Emmett Shear"))
+        lessons.append(HTSASLesson(title: "Legal and Accounting Basics for Startups", authors: "Kirsty Nathoo, Carolynn Levei"))
+        lessons.append(HTSASLesson(title: "How to Talk Investors", authors: "Tyler Bosmeny"))
+        lessons.append(HTSASLesson(title: "Later-Stage Advice", authors: "Sam Altman"))
         
         tableView.dataSource = self
     }
