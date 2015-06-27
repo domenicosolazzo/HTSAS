6f863c77b1b154ab40ee9961507ab0dbe784b84c
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-27 21:28:20 +0200

    Adding detail view

---
+++ HTSAS/HTSAS/LessonViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LessonViewController.swift
+//  HTSAS
+//
+//  Created by Domenico on 27/06/15.
+//  Copyright (c) 2015 Domenico Solazzo. All rights reserved.
+//
+
+import Cocoa
+
+class LessonViewController: UIViewController {
+
+    @IBOutlet weak var lessonTitle: UILabel!
+    @IBOutlet weak var lessonAuthors: UILabel!
+}
