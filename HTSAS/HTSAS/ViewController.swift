f3f5ad5bb05c434d0d052b2572c4bb95ba4b7cac
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-27 20:15:36 +0200

    Adding table view with list of lessons

--- HTSAS/HTSAS/ViewController.swift
+++ HTSAS/HTSAS/ViewController.swift
@@ -8,16 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    private var lessons = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        lessons.append("Lesson 1")
+        lessons.append("Lesson 2")
+        println(lessons)
+        
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //-MARK: TableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return lessons.count
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("lessonCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        var data: String = lessons[indexPath.row]
+        println(data)
+        cell.textLabel!.text = data
+        
+        return cell
     }
 
 
