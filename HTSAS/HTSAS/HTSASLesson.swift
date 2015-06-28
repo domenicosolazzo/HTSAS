862c049fc83e964709bbdd2112302657c5e33a79
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-28 08:33:23 +0200

    Adding new properties to the HTSASLesson class

--- HTSAS/HTSAS/HTSASLesson.swift
+++ HTSAS/HTSAS/HTSASLesson.swift
@@ -11,6 +11,8 @@ import Foundation
 public class HTSASLesson:NSObject{
     public var title = ""
     public var authors = ""
+    public var videoUrl = ""
+    public var transcripts = [String]()
     
     override init(){
         super.init()
@@ -20,6 +22,14 @@ public class HTSASLesson:NSObject{
         self.init()
         self.title = title
         self.authors = authors
+    }
+    
+    convenience init(title: String, authors: String, videoUrl: String, transcripts: [String]){
+        self.init()
+        self.title = title
+        self.authors = authors
+        self.videoUrl = videoUrl
+        self.transcripts = transcripts
     }
     
     
