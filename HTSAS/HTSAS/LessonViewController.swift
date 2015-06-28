22b151fa25818429c5dcecd3a7feda8c362e25a7
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-28 09:13:53 +0200

    Connecting the tableView data source to the class

--- HTSAS/HTSAS/LessonViewController.swift
+++ HTSAS/HTSAS/LessonViewController.swift
@@ -8,16 +8,20 @@
 
 import UIKit
 
-class LessonViewController: UIViewController {
+class LessonViewController: UIViewController, UITableViewDataSource {
 
     private var theTitle: String = "ciao"
     private var authors: String = "ciao"
+    private var videoUrl: String = ""
+    private var transcripts = [String]()
     
     @IBOutlet weak var lessonTitle: UILabel!
     @IBOutlet weak var lessonAuthors: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         
         self.lessonTitle.text = theTitle
         self.lessonAuthors.text = authors
@@ -31,6 +35,8 @@ class LessonViewController: UIViewController {
     }
     
     func configureView() {
+        
+        
         // Update the user interface for the detail item.
         if let detail: HTSASLesson = self.detailLessonItem as? HTSASLesson {
             if self.lessonTitle == nil{
@@ -38,6 +44,27 @@ class LessonViewController: UIViewController {
             }
             self.theTitle = detail.title
             self.authors = detail.authors
+            self.transcripts = detail.transcripts
+            self.videoUrl = detail.videoUrl
         }
+    }
+    
+    @IBAction func playVideo(sender: AnyObject) {
+    }
+    
+    //-MARK: UITableViewDataSource
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return transcripts.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("transcriptCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        cell.textLabel!.text = transcripts[indexPath.row]
+        return cell
     }
 }
