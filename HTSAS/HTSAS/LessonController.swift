8093e4eb88a1ad870c971c301413e33f58a172e5
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-07-02 07:44:04 +0200

    Adding images; refactoring: using the tableview controller for the transcripts

---
+++ HTSAS/HTSAS/LessonController.swift
@@ -0,0 +1,13 @@
+//
+//  LessonController.swift
+//  HTSAS
+//
+//  Created by Domenico on 01/07/15.
+//  Copyright (c) 2015 Domenico Solazzo. All rights reserved.
+//
+
+import Cocoa
+
+class LessonController: UITableViewController {
+
+}
