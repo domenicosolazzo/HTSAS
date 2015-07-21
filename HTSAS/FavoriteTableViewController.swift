94ca13954b2bde5f60422239838cf7daa6df5735
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-07-21 07:55:23 +0200

    TableViewController class for the Favourite class

---
+++ HTSAS/FavoriteTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FavoriteTableViewController.swift
+//  HTSAS
+//
+//  Created by Domenico on 21/07/15.
+//  Copyright (c) 2015 Domenico Solazzo. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteTableViewController: UITableViewController {
+
+}
