ad0e6109567f3cc85e404150153ba10fd411e912
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-07-24 19:37:59 +0200

    Adding an intro controllcontroller

---
+++ HTSAS/HTSAS/IntroViewController.swift
@@ -0,0 +1,13 @@
+//
+//  IntroViewController.swift
+//  HTSAS
+//
+//  Created by Domenico on 24/07/15.
+//  Copyright (c) 2015 Domenico Solazzo. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+}
