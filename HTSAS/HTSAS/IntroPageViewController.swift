c1cced431a1d4842601645d20bac18e25baf0491
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-07-27 15:03:21 +0200

    Adding class for the PageViewController

---
+++ HTSAS/HTSAS/IntroPageViewController.swift
@@ -0,0 +1,14 @@
+
+//
+//  IntroPageViewController.swift
+//  HTSAS
+//
+//  Created by Domenico on 27/07/15.
+//  Copyright (c) 2015 Domenico Solazzo. All rights reserved.
+//
+
+import Cocoa
+
+class IntroPageViewController: UIPageViewController {
+
+}
